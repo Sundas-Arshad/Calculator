c38ff8a511650b2a42bb10b0cef329be6d3d494b
Sundas-Arshad <Sundas-Arshad>
2024-02-18 17:41:52 -0800

    Changes in View

---
+++ View/index.js
@@ -0,0 +1,24 @@
+function calculate(operator) {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = CalculatorModel.add(num1, num2);
+      break;
+    case '-':
+      result = CalculatorModel.subtract(num1, num2);
+      break;
+    case '*':
+      result = CalculatorModel.multiply(num1, num2);
+      break;
+    case '/':
+      result = CalculatorModel.divide(num1, num2);
+      break;
+    default:
+      result = 'Error: Invalid operator';
+  }
+
+  document.getElementById('result').innerText = result;
+}
