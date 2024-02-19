b7bbbf56574f535740ffbc4611f6aa0a0a06a161
Sundas-Arshad <Sundas-Arshad>
2024-02-18 17:39:36 -0800

    Changes in model

---
+++ Model/index.js
@@ -0,0 +1,6 @@
+const CalculatorModel = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (b !== 0 ? a / b : 'Error: Division by zero'),
+};
